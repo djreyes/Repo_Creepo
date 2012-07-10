f21a0ca91e4f66489f0f20f0f7ca1c7d8521933d
Jeremy Reyes + Mary Zhu <apprentice+jr+mz@devbootcamp.com>
2012-07-10 16:20:39 -0700

    Added a method_counter and a method_bundler + tests

--- repo_report.rb
+++ repo_report.rb
@@ -2,11 +2,28 @@ require './repo_method.rb'
 
 module RepoCreepo
   class RepoReport
-    attr_reader :repo_id, :methods
+    attr_reader :repo_id, :repo_methods
     def initialize(repo_id = nil)
         @repo_id = repo_id
-        @methods = []
+        @repo_methods = []
     end
 
+    def method_counter(array_of_methods)
+      method_hash = {}
+      array_of_methods.each do |element|
+        if method_hash[element].nil?
+          method_hash[element] = 1
+        else
+          method_hash[element] += 1
+        end
+      end
+      method_hash
+    end
+
+    def method_bundler(hash_of_methods)
+      hash_of_methods.each do |key, value|
+        @repo_methods << RepoMethod.new(key, value)
+      end
+    end
   end
 end
--- repo_report_spec.rb
+++ repo_report_spec.rb
@@ -9,20 +9,66 @@ describe 'RepoCreepo::RepoReport' do
     it 'is an instance of RepoReport' do
       @repo_report.should be_an_instance_of RepoReport
     end
-    it "has nil repo_id" do
+    it 'has nil repo_id' do
       @repo_report.repo_id.should be_nil
     end
-    it "has @methods array" do
-      @repo_report.methods.should eq []
+    it 'has @repo_methods array' do
+      @repo_report.repo_methods.should eq []
     end
   end
 
   context '#self.from_dir' do
-    it "" do
+    it 'should create a report from a directory'
+  end
 
+  context '#method_counter' do
+    before (:each) {@my_hash = @repo_report.method_counter([:get, :push, :each, :push, :push])}
+    it 'should return a hash' do
+      @my_hash.should be_an_instance_of Hash
+    end
+
+    it 'should have counts for each element of the array' do
+      @my_hash[:get].should eq 1
+      @my_hash[:push].should eq 3
     end
   end
 
+  context '#method_bundler' do
+    before (:each) {@method_hash = @repo_report.method_counter([:get, :push, :each, :push, :push])}
 
+    # @method_hash[:get] => 1
+    # @method_hash[:push] => 3
+    # @method_hash[:each] => 1
+    #
+    # RepoMethod.new(:get, 1)
+    # RepoMethod.new(:push, 3)
+
+    it 'should load @repo_methods with method objects' do
+      @repo_report.method_bundler(@method_hash)
+      @repo_report.repo_methods.should_not be_empty
+    end
+
+    it 'should have method objects with counts' do
+      @repo_report.method_bundler(@method_hash)
+      @repo_report.repo_methods[0].should be_an_instance_of RepoMethod
+    end
+
+    it 'should store the counts of each method in the @repo_methods array' do
+      @repo_report.method_bundler(@method_hash)
+      # RepoMethod.new(:get, @method_hash[:get])
+      # :get.count.should eq 1
+
+      # @method_hash = @repo_report.method_counter(array_from_dk)
+      # @repo_report.method_bundler(@method_hash)
+      # repo_methods gets filled with something as a result
+      # @repo_reports.repo_methods holds what we expect
+      first_element_name = @repo_report.repo_methods[0].name
+      method_count = @method_hash[first_element_name]
+      @repo_report.repo_methods[0].count.should eq method_count
+
+    end
+
+
+  end
 
 end
